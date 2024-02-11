bd9902ad16056cb0bffaa75c9a0c5f8bf278f36e
RobinDev <contact@piedweb.com>
2024-02-11 13:48:31 +0000

    fix webpack encore and upgrade front dependencies

--- webpack.config.js
+++ webpack.config.js
@@ -1,27 +1,17 @@
-const EncoreHelper = require('@pushword/js-helper/src/encore.js');
+const EncoreHelper = require('@pushword/js-helper/src/encore.js')
+const Encore = require('@symfony/webpack-encore')
 
 var watchFiles = [
-    './src/templates/**/*.html.twig',
-    './src/templates/*.html.twig',
-    './../conversation/src/templates/conversation/*.html.twig',
-    './../admin-block-editor/src/templates/block/*.html.twig',
-    './../advanced-main-image/src/templates/page/*.html.twig',
-    './../js-helper/src/helpers.js',
-];
+  './src/templates/**/*.html.twig',
+  './src/templates/*.html.twig',
+  './../conversation/src/templates/conversation/*.html.twig',
+  './../admin-block-editor/src/templates/block/*.html.twig',
+  './../advanced-main-image/src/templates/page/*.html.twig',
+  './../js-helper/src/helpers.js',
+]
 
-var tailwindConfig = EncoreHelper.getTailwindConfig(watchFiles);
+var tailwindConfig = EncoreHelper.getTailwindConfig(watchFiles)
 
+EncoreHelper.getEncore(Encore, watchFiles, tailwindConfig, __dirname + '/src/Resources/public', '/bundles/pushwordcore', 'bundles/pushwordcore', [])
 
-module.exports = EncoreHelper.getEncore(
-    watchFiles,
-    tailwindConfig,
-    './src/Resources/public/',
-    './',
-    'bundles/pushwordcore',
-    [
-        {
-            from: './src/Resources/assets/favicons',
-            to: 'favicons/[name].[ext]',
-        },
-    ]
-).getWebpackConfig();
+module.exports = Encore.getWebpackConfig()
